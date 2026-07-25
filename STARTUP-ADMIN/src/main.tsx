2a645b1ea08d84db26c38adffcaaa3eefd8d2204
abhishek711p-pixel <abhishek.711p@gmail.com>
2026-07-25 20:10:48 +0530

    Integrate Vercel Web Analytics in student and admin apps

--- STARTUP-ADMIN/src/main.tsx
+++ STARTUP-ADMIN/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AdminDashboard from './components/AdminDashboard';
 import './index.css';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -9,5 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       // Redirect back to main site on exit
       window.location.href = 'http://localhost:5175/';
     }} />
+    <Analytics />
   </React.StrictMode>
 );
